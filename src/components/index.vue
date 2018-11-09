77d46625b3b81b7d95d8f9f2601af47d2876bdfc
kaiw <kaiw@lily.sh.cn>
2018-11-09 22:18:16 +0800

    Initial commit

---
+++ src/components/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <mu-container>
+    <mu-text-field label="请输入IP地址" v-model="ipaddr"></mu-text-field>
+    <mu-button @click="openNewPage">打开新链接</mu-button>
+  </mu-container>
+</template>
+
+<script>
+    export default {
+      name: "index",
+      data() {
+        return {
+          ipaddr: ''
+        }
+      },
+      methods: {
+        openNewPage() {
+          window.location.href = `http://${this.ipaddr}:8088`
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
